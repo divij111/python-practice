cee8a3a80fc3473e0f604fba65fd7be1762f968d
divij111 <goyaldivij111@gmail.com>
2021-09-01 16:42:02 +0530

    Initial commit

---
+++ sources/wish.py
@@ -0,0 +1,12 @@
+import os
+import datetime
+time = datetime.datetime.now()
+now = int(time.strftime("%H"))
+if now>=0 and now<=11:
+    print("Hello Good Morning!")
+elif now>=12 and now<=15:
+    print("Hello Good Afternoon!")
+elif now>=16 and now<=19:
+    print("Hello Good Evening!")
+else:
+    print("Hello Good Night!")
